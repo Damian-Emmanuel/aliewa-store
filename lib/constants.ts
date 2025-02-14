f315c65f2ed0a86c2db3420d4f2df6370259e873
Emmanuel-Damian <damianemmanuel82@gmail.com>
2025-02-14 11:46:09 +0100

    03-create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'aliewa-store'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Best of Qualities with Competitive prices'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An E-commerce Web App built with Next.js and MongoDB'
